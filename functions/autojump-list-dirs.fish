adb9d05721b7d2e94f88f3b46b776eb2e1be1f71
Thomas Liebeskind <thomas.liebeskind@gmail.com>
2020-08-28 21:42:56 +0200

    make it work on macos

--- functions/autojump-list-dirs.fish
+++ functions/autojump-list-dirs.fish
@@ -1,3 +1,3 @@
 function autojump-list-dirs
-  autojump -s | head -n -7 | sort -n | tac | awk  '{$1=""; print $0}' | awk '{$1=$1};1'
+  autojump -s | tail -r | tail -n +8 | tail -r | sort -n | tac | awk  '{$1=""; print $0}' | awk '{$1=$1};1'
 end
